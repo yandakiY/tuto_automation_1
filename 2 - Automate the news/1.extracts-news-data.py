73ce11bb22f828603137e0840a6744bb9edacead
yandaki <boayves@gmail.com>
2023-12-27 19:19:03 +0000

    2- extracts news data

--- 2 - Automate the news/1.extracts-news-data.py
+++ 2 - Automate the news/1.extracts-news-data.py
@@ -12,5 +12,21 @@ service = Service(executable_path = path)
 # # instance driver
 driver = webdriver.Chrome()
 
-# get data
+# print the website
 driver.get(website)
+
+
+# find specify element
+containers = driver.find_elements(by='xpath' , value="//div[@class = 'teaser__copy-container']")
+
+list_title = []
+
+
+for container in containers:
+
+    title = driver.find_element(by='xpath' , value="./a/span[contains(@class , 'teaser__headline')]").text
+    sub_title = driver.find_element(by='xpath' , value="./a/h3").text
+    link = driver.find_element(by='xpath' , value='./a').get_attribute('href')
+    #  list_title.append(title)
+
+print(containers.count())
